25510ae8f70e651848cf1b90fb5b8c709e935ec5
Abdul De Borja <abduldbdev@gmail.com>
2026-09-04 17:24:35 +0800

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,6 +54,7 @@ module.exports.loginUser = (req, res) => {
       }
 
       return res.status(200).send({
+        userId: result._id,
         access: auth.createAccessToken(result),
       });
     })
